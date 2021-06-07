4ea6a57222f5fb76c468727eafd059677f4deed9
José Carlos de Freitas <jcfreitas2000@hotmail.com>
2021-06-07 07:57:40 -0300

    adds font Itallo

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,6 +1,13 @@
 import { extendTheme } from '@chakra-ui/react'
 
 export const theme = extendTheme({
+    colors: {
+
+    },
+    fonts: {
+        heading: 'Inter',
+        body: 'Inter'
+    },
     styles: {
         global: {
             body: {
