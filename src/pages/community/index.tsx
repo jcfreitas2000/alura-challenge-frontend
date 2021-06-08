23987739e5ec4c797628e4009fb1e19f51404ad7
José Carlos de Freitas <jcfreitas2000@hotmail.com>
2021-06-08 17:48:15 -0300

    title, meta description and favicon

--- src/pages/community/index.tsx
+++ src/pages/community/index.tsx
@@ -1,10 +1,15 @@
 import {Layout} from "../../components/Layout";
 import {Grid, GridItem} from "@chakra-ui/react";
 import {CodeCard} from "../../components/CodeCard";
+import Head from 'next/head';
 
 export default function community() {
     return (
         <Layout>
+            <Head>
+                <title>Comunidade</title>
+                <meta name="description" content="Página da comunidade da plataforma Alura Dev" />
+            </Head>
             <Grid maxW="100%" templateColumns="1fr 1fr" gap={6}>
                 <GridItem>
                     <CodeCard color="red.400" title="Título" description="Descrição" commentsCount={4} likesCount={5}/>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,19 @@
 import {ProjectForm} from "../components/Project/Form";
 import {Code} from "../components/Code";
 import {Layout} from "../components/Layout";
+import Head from 'next/head';
 
 export default function Home() {
     return (
-        <Layout>
-            <Code/>
-            <ProjectForm/>
-        </Layout>
+        <>
+            <Head>
+                <title>Home</title>
+                <meta name="description" content="Página inicial da plataforma Alura Dev"/>
+            </Head>
+            <Layout>
+                <Code/>
+                <ProjectForm/>
+            </Layout>
+        </>
     )
 }
