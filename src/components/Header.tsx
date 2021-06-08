948625abe872623b7f3c500ab6b0f598bff2e8c3
José Carlos de Freitas <jcfreitas2000@hotmail.com>
2021-06-08 17:35:09 -0300

    adds working links

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,24 @@
-import {Box, Flex, HStack, Image, Text} from "@chakra-ui/react";
+import {Box, HStack, Image, Link as ChakraLink, Text} from "@chakra-ui/react";
 import {Input} from "./Form/Input";
 import {Profile} from "./Profile";
+import Link from "next/link"
 
 export function Header() {
     return (
         <HStack p={8} justify="space-between" spacing={8}>
             <Box w="100%" maxW="300px">
-                <Image src="/images/logo.svg" alt="Logo Alura dev"/>
+                <Link href="/">
+                    <ChakraLink>
+                        <Image src="/images/logo.svg" alt="Alura Dev"/>
+                        <Text as="h1" hidden={true}>Alura Dev</Text>
+                    </ChakraLink>
+                </Link>
             </Box>
             <Box as="form" w="100%">
-                <Input name="search" placeholder="Busque por algo" />
+                <Input name="search" placeholder="Busque por algo"/>
             </Box>
 
-            <Profile name="Zezin do Cross" />
+            <Profile name="Zezin do Cross"/>
         </HStack>
     );
 }
--- src/components/SidebarMenu.tsx
+++ src/components/SidebarMenu.tsx
@@ -1,13 +1,15 @@
-import {Box, Flex, HStack, Image, List, ListItem, Stack, Text} from "@chakra-ui/react";
+import {Box, Flex, HStack, Image, List, ListItem, Text, Link as ChakraLink} from "@chakra-ui/react";
 import {Subtitle} from "./Text/Subtitle";
+import Link from "next/link"
+import {useRouter} from "next/router";
 
 export function SidebarMenu() {
     return (
         <Flex direction="column" w="100%" maxW="300px">
             <Subtitle text="Menu" as="small"/>
             <List spacing={4} mt={4}>
-                <MenuItem iconSrc="/icons/code.svg" text="Editor de código" active={true} />
-                <MenuItem iconSrc="/icons/users.svg" text="Comunidade" active={false} />
+                <MenuItem iconSrc="/icons/code.svg" text="Editor de código" href="/"/>
+                <MenuItem iconSrc="/icons/users.svg" text="Comunidade" href="/community"/>
             </List>
         </Flex>
     )
@@ -16,18 +18,25 @@ export function SidebarMenu() {
 interface MenuItemProps {
     iconSrc: string;
     text: string;
-    active: boolean;
+    href: string;
 }
 
-function MenuItem({iconSrc, text, active}: MenuItemProps) {
+function MenuItem({iconSrc, text, href}: MenuItemProps) {
+    const router = useRouter();
+
     return (
         <ListItem>
-            <HStack>
-                <Box p={3} borderRadius="1rem" bgColor="blue.500" filter={active ? '' : 'brightness(0.5)'}>
-                    <Image w={6} src={iconSrc} alt="Ícone de código"/>
-                </Box>
-                <Text>{text}</Text>
-            </HStack>
+            <Link href={href}>
+                <ChakraLink>
+                    <HStack>
+                        <Box p={3} borderRadius="1rem" bgColor="blue.500"
+                             filter={router.pathname === href ? '' : 'brightness(0.5)'}>
+                            <Image w={6} src={iconSrc} alt="Ícone de código"/>
+                        </Box>
+                        <Text>{text}</Text>
+                    </HStack>
+                </ChakraLink>
+            </Link>
         </ListItem>
     );
 }
