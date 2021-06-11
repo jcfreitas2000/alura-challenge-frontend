e7ae704ade21c3ce38c11398d2cc731c1129e5e5
José Carlos de Freitas <jcfreitas2000@hotmail.com>
2021-06-11 08:30:46 -0300

    creating color picker barrel

--- src/components/Form/ColorPicker/index.tsx
+++ src/components/Form/ColorPicker/index.tsx
@@ -1,22 +1,4 @@
-import {
-    Box,
-    Button,
-    Center,
-    Popover,
-    PopoverArrow,
-    PopoverBody,
-    PopoverCloseButton,
-    PopoverContent,
-    PopoverTrigger,
-    Text,
-    Tooltip,
-    useRadio,
-    useRadioGroup,
-    UseRadioProps,
-    Wrap
-} from '@chakra-ui/react'
-
-type ColorLabel =
+export type ColorLabel =
     | 'Cinza'
     | 'Vermelho'
     | 'Laranja'
@@ -40,12 +22,12 @@ export type ColorValue =
     | 'purple.400'
     | 'pink.400'
 
-type Color = {
+export type Color = {
     label: ColorLabel
     value: ColorValue
 }
 
-const options: Color[] = [
+export const colorsOptions: Color[] = [
     { label: 'Cinza', value: 'gray.400' },
     { label: 'Vermelho', value: 'red.400' },
     { label: 'Laranja', value: 'orange.400' },
@@ -58,148 +40,4 @@ const options: Color[] = [
     { label: 'Rosa', value: 'pink.400' }
 ]
 
-interface ColorPickerProps {
-    color: ColorValue
-    setColor: (color: ColorValue) => void
-}
-
-export function ColorPicker({
-    color,
-    setColor
-}: ColorPickerProps): JSX.Element {
-    const popoverBg = 'blue.900'
-    const selectedColor =
-        options.find(option => color === option.value) || options[0]
-
-    return (
-        <Popover>
-            <PopoverTriggerColor selectedColor={selectedColor} />
-            <PopoverContent bg={popoverBg}>
-                <PopoverArrow bg={popoverBg} />
-                <PopoverCloseButton />
-                <PopoverBody>
-                    <RadioGroupColor
-                        colors={options}
-                        selectedColor={selectedColor}
-                        setSelectedColor={color => setColor(color.value)}
-                    />
-                </PopoverBody>
-            </PopoverContent>
-        </Popover>
-    )
-}
-
-interface PopoverTriggerColorProps {
-    selectedColor: Color
-}
-
-function PopoverTriggerColor({ selectedColor }: PopoverTriggerColorProps) {
-    return (
-        <PopoverTrigger>
-            <Button
-                width="100%"
-                p={1}
-                py={6}
-                borderRadius="md"
-                borderWidth={1}
-                bg="transparent"
-                _hover={{
-                    bg: 'inherit',
-                    filter: 'brightness(0.75)'
-                }}
-                _active={{
-                    bg: 'inherit',
-                    filter: 'brightness(0.5)'
-                }}
-                _before={{
-                    content: '""',
-                    display: 'block',
-                    bg: selectedColor.value,
-                    width: '100%',
-                    height: 10,
-                    borderRadius: 'md'
-                }}
-            >
-                <Text hidden>{selectedColor.label}</Text>
-            </Button>
-        </PopoverTrigger>
-    )
-}
-
-interface RadioGroupColorProps {
-    colors: Color[]
-    selectedColor: Color
-    setSelectedColor: (color: Color) => void
-}
-
-function RadioGroupColor({
-    colors,
-    selectedColor,
-    setSelectedColor
-}: RadioGroupColorProps) {
-    const { getRootProps, getRadioProps } = useRadioGroup({
-        name: 'color',
-        defaultValue: selectedColor.value,
-        onChange: colorValue => {
-            const color = colors.find(color => color.value === colorValue)
-            if (color) setSelectedColor(color)
-        }
-    })
-
-    const group = getRootProps()
-
-    return (
-        <Wrap spacing={1} {...group}>
-            {colors.map((color: Color) => {
-                const radio = getRadioProps(color)
-                return (
-                    <RadioItemColor
-                        key={color.value}
-                        colorItem={color}
-                        {...radio}
-                    />
-                )
-            })}
-        </Wrap>
-    )
-}
-
-interface RadioItemColorProps extends UseRadioProps {
-    colorItem: Color
-}
-
-function RadioItemColor({ colorItem, ...props }: RadioItemColorProps) {
-    const { getInputProps, getCheckboxProps } = useRadio(props)
-
-    const input = getInputProps()
-    const checkbox = getCheckboxProps()
-
-    return (
-        <Tooltip
-            hasArrow
-            label={colorItem.label}
-            bg={colorItem.value}
-            color="black"
-        >
-            <Box as="label">
-                <input {...input} />
-                <Center
-                    {...checkbox}
-                    cursor="pointer"
-                    bg={colorItem.value}
-                    w={7}
-                    h={7}
-                    borderRadius="50%"
-                    m={1}
-                    _checked={{
-                        _after: {
-                            content: '"✓"',
-                            display: 'block',
-                            color: 'black'
-                        }
-                    }}
-                />
-            </Box>
-        </Tooltip>
-    )
-}
+export { ColorPicker } from './ColorPicker'
